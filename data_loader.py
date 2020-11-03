bcbe6534a2c7eb77f503bbe8ca2d087e0056b403
Kevin Laemers <18590570+PloxKevin@users.noreply.github.com>
2020-11-03 03:23:27 +0100

    Dataloader edits

--- data_loader.py
+++ data_loader.py
@@ -1,27 +1,18 @@
-import pandas as pd
-import os
 import torch
 from skimage import io, transform
 import numpy as np
-import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
 
-
+experiment = "datasets/split/60.csv"
 
 class PartialDataset(Dataset):
-#take 2 directory as argument
-#is input img, can be train/test
-# train val test
     def __init__(self, mnist_dir):
-        self.examples = np.loadtxt("datasets/split/60.csv", delimiter=",")
+        self.examples = np.loadtxt(experiment, delimiter=",")
         self.pre_selections = np.load(mnist_dir)
         print(self.pre_selections.shape)
     def __len__(self):
-
         assert self.examples.shape[0] == self.pre_selections.shape[0]
         return self.examples.shape[0]
-
     def __getitem__(self, item): #item = index
         input_img = self.examples[item, 1:].reshape(1, 28, 28) / 255.
         #(1,28,28) here or in the training
@@ -41,36 +32,36 @@ class PartialDataset(Dataset):
 
         return example
 
-#class PriorDataset(Dataset):
+class PriorDataset(Dataset):
 
-#    def __init__(self):
-#        self.examples = np.loadtxt("datasets/mnist_combined.csv", delimiter=",")[5000:]
+    def __init__(self):
+        self.examples = np.loadtxt(experiment, delimiter=",")
 
-#    def __len__(self):
-#        return self.examples.shape[0]
+    def __len__(self):
+        return self.examples.shape[0]
 
-#    def __getitem__(self, item):
-#        input_img = self.examples[item, 1:].reshape(28, 28) / 255.
+    def __getitem__(self, item):
+        input_img = self.examples[item, 1:].reshape(28, 28) / 255.
         # input_img[9:19, 9:19] = 0.5
 
-#        input_img =  transform.resize(input_img, (64, 64), preserve_range=True)
-#        input_img = torch.from_numpy(np.expand_dims(input_img, 0))
+        input_img =  transform.resize(input_img, (64, 64), preserve_range=True)
+        input_img = torch.from_numpy(np.expand_dims(input_img, 0))
 
-#        example = {
-#                   'prior': input_img,
-#                  }
+        example = {
+                   'prior': input_img,
+                 }
 
-#        return example
+        return example
 
 
 
 if __name__ == '__main__':
-    dataset = PartialDataset('datasets/split/preselection_top1_60.npy')
+    #dataset = PartialDataset('datasets/split/preselection_top1_60.npy')
 
-    print(len(dataset))
-    print(dataset[0]['input'])
-    print(dataset[0]['pre_sele'])
-
-    #dataset = PriorDataset()
     #print(len(dataset))
-    #print(dataset[0]['prior'])
+    #print(dataset[0]['input'])
+    #print(dataset[0]['pre_sele'])
+
+    dataset = PriorDataset()
+    print(len(dataset))
+    print(dataset[0]['prior'])
