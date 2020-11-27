f5d1f5f4308e97b8fd78c94ccb54e44effb3c9f3
Kevin Laemers <18590570+PloxKevin@users.noreply.github.com>
2020-11-27 16:11:55 +0100

    Update custom_eval.py

--- custom_eval.py
+++ custom_eval.py
@@ -3,23 +3,21 @@ import glob
 import numpy as np
 from skimage import io
 from tqdm import tqdm
-
-
-mode = 'wo_12'
-prediction_dir = 'datasets/predictions/'
+import re
+prediction_dir = 'datasets/predictions/top16/'
 gt_list = sorted(glob.glob(prediction_dir + '*_gt.png'))
-pred_list = sorted(glob.glob(prediction_dir + '*_pred_' + mode + '.png'))
+pred_list = sorted(glob.glob(prediction_dir + '*_pred.png'))
 
-gt = np.zeros([10000, 64, 64])
-pred = np.zeros([10000, 64, 64])
+gt = np.zeros([500, 64, 64])
+pred = np.zeros([500, 64, 64])
 for i in tqdm(range(len(gt_list))):
     gt[i, :, :] = io.imread(gt_list[i]) / 255.
 
 for i in tqdm(range(len(pred_list))):
     pred[i, :, :] = io.imread(pred_list[i]) / 255.
 
-l2 = (gt - pred) * (gt- pred)
+l2 = (gt - pred) * (gt - pred)
 print(l2.shape)
 print(np.min(l2))
 l2_avg = np.mean(l2)
-print(mode, l2_avg)
+print(l2_avg)
