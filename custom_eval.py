79bd8690309ec161c5e0d0a5715dfa61f7baf786
PloxKevin <k.laemersdev@gmail.com>
2020-12-06 19:00:33 +0100

    Update custom_eval.py

--- custom_eval.py
+++ custom_eval.py
@@ -4,20 +4,24 @@ import numpy as np
 from skimage import io
 from tqdm import tqdm
 import re
-prediction_dir = 'datasets/predictions/top16/'
-gt_list = sorted(glob.glob(prediction_dir + '*_gt.png'))
-pred_list = sorted(glob.glob(prediction_dir + '*_pred.png'))
 
-gt = np.zeros([500, 64, 64])
-pred = np.zeros([500, 64, 64])
-for i in tqdm(range(len(gt_list))):
-    gt[i, :, :] = io.imread(gt_list[i]) / 255.
+top_ranges = [1,2,4,8,16,32]
 
-for i in tqdm(range(len(pred_list))):
-    pred[i, :, :] = io.imread(pred_list[i]) / 255.
+for top in top_ranges:
 
-l2 = (gt - pred) * (gt - pred)
-print(l2.shape)
-print(np.min(l2))
-l2_avg = np.mean(l2)
-print(l2_avg)
+    prediction_dir = 'datasets/predictions/top'+ str(top) +'/'
+    gt_list = sorted(glob.glob(prediction_dir + '*_um.png'))
+    pred_list = sorted(glob.glob(prediction_dir + '*_pred.png'))
+
+    gt = np.zeros([500, 64, 64])
+    pred = np.zeros([500, 64, 64])
+    for i in tqdm(range(len(gt_list))):
+        gt[i, :, :] = io.imread(gt_list[i]) / 255.
+
+    for i in tqdm(range(len(pred_list))):
+        pred[i, :, :] = io.imread(pred_list[i]) / 255.
+
+    l2 = (gt - pred) * (gt - pred)
+    l2_avg = np.mean(l2)
+    print('top'+str(top), l2_avg)
+
