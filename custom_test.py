28255ec6740cc66a001de05e8579aea28cf2825c
PloxKevin <k.laemersdev@gmail.com>
2020-12-06 18:51:57 +0100

    Prediction Mapping updated

--- custom_test.py
+++ custom_test.py
@@ -24,7 +24,7 @@ torch.cuda.manual_seed(seed)
 #top1: 0.035490142822265626
 
 test_csv = "datasets/split/10.csv"
-test_pre = "datasets/split/preselection_top1_10.npy"
+test_pre = "datasets/split/preselection_top32_10.npy"
 
 with_msk_channel = False
 result = re.search('preselection_(.*)_', test_pre)
@@ -53,7 +53,7 @@ for i, temp_batch in enumerate(test_loader):
         print('example no. ', i)
     temp_input_img = temp_batch['input'].float().to(device)
     unmasked = temp_batch['unmasked'].float().to(device)
-
+    temp_ground_truth = temp_batch['pre_sele'].float().to(device)
     # forward
     # track history if only in train
     with torch.set_grad_enabled(False):
@@ -63,15 +63,15 @@ for i, temp_batch in enumerate(test_loader):
             pred_map = G(temp_input_img, False)
 
         # torch_img_visualization(2, [pred_map.detach(), temp_map_input.detach()])
+        io.imsave('datasets/predictions/'+result.group(1)+'/{0:06d}_um.png'.format(i),
+                  (unmasked.cpu().numpy() * 255).astype(np.uint8).reshape((64, 64)))
         io.imsave('datasets/predictions/'+result.group(1)+'/{0:06d}_gt.png'.format(i),
-                  (.cpu().numpy() * 255).astype(np.uint8).reshape((64, 64)))
-        #io.imsave('datasets/predictions/'+result.group(1)+'/{0:06d}_gt.png'.format(i),
-        #          (temp_ground_truth.cpu().numpy() * 255).astype(np.uint8).reshape((64, 64)))
-        #io.imsave('datasets/predictions/'+result.group(1)+'/{0:06d}_input.png'.format(i),
-        #          (temp_input_img.cpu().numpy() * 255).astype(np.uint8).reshape((64, 64)))
-        #io.imsave('datasets/predictions/'+result.group(1)+'/{0:06d}_pred.png'.format(i),
-        #         (pred_map.cpu().numpy() * 255).astype(np.uint8).reshape((64, 64)))
-        #loss_input_pred += F.mse_loss(temp_input_img, pred_map)
+                  (temp_ground_truth.cpu().numpy() * 255).astype(np.uint8).reshape((64, 64)))
+        io.imsave('datasets/predictions/'+result.group(1)+'/{0:06d}_input.png'.format(i),
+                  (temp_input_img.cpu().numpy() * 255).astype(np.uint8).reshape((64, 64)))
+        io.imsave('datasets/predictions/'+result.group(1)+'/{0:06d}_pred.png'.format(i),
+                 (pred_map.cpu().numpy() * 255).astype(np.uint8).reshape((64, 64)))
+        loss_input_pred += F.mse_loss(temp_input_img, pred_map)
         loss_gt_pred += F.mse_loss(unmasked, pred_map)
 print(loss_gt_pred.item()/len(test_set))
 
